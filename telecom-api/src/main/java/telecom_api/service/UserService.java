b3056a584fde0b6b848faa3ca091587bb0fa0a89
Ramalingam-Rajagopal <ramalingam15102002@gmail.com>
2026-04-17 12:10:22 +0530

    modified user controller and service to add PutMapping endpoint

--- telecom-api/src/main/java/telecom_api/service/UserService.java
+++ telecom-api/src/main/java/telecom_api/service/UserService.java
@@ -2,7 +2,11 @@ package telecom_api.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import telecom_api.mapper.UserMapper;
+import telecom_api.dto.UserRequestDTO;
+import telecom_api.dto.UserResponseDTO;
 import telecom_api.entity.User;
+import telecom_api.exception.ResourceNotFoundException;
 import telecom_api.repository.UserRepository;
 
 import java.util.List;
@@ -32,5 +36,18 @@ public class UserService {
 
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
+    }
+
+    public UserResponseDTO updateUser(Long id, UserRequestDTO dto) {
+        User existingUser = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+
+        existingUser.setFullName(dto.getFullName());
+        existingUser.setEmail(dto.getEmail());
+        existingUser.setPassword(dto.getPassword());
+        existingUser.setRole(dto.getRole());
+
+        User updatedUser = userRepository.save(existingUser);
+
+        return UserMapper.toResponseDTO(updatedUser);
     }
 }
